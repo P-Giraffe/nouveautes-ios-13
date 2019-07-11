44d10aa6ed4e9ff97a3d5f59c34d764fa45bcd44
Maxime Britto <maxime@purplegiraffe.fr>
2019-07-11 16:31:35 +0200

    Added base project for Dark mode

---
+++ DarkMode/DarkMode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DarkMode
+//
+//  Created by Maxime Britto on 11/07/2019.
+//  Copyright © 2019 Maxime Britto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
