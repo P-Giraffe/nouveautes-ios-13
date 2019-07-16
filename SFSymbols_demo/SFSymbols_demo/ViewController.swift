21f9c93f8178f26cbfe6ff0eed5315dc0e7b322e
Maxime Britto <maxime@purplegiraffe.fr>
2019-07-16 18:51:21 +0200

    145_Personnaliser_laffichage_du_symbole

--- SFSymbols_demo/SFSymbols_demo/ViewController.swift
+++ SFSymbols_demo/SFSymbols_demo/ViewController.swift
@@ -17,8 +17,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         guard let tornado = UIImage(systemName: "tornado") else { return }
+        
+        let symbolConf = UIImage.SymbolConfiguration(pointSize: 34.0, weight: .heavy)
+        
         imageView.image = tornado
+        imageView.preferredSymbolConfiguration = symbolConf
+        
         button.setImage(UIImage(systemName: "snow"), for: .normal)
+        button.setPreferredSymbolConfiguration(symbolConf, forImageIn: .normal)
+        
+        let attributedString = NSMutableAttributedString(string: "Dans Twister, ils aiment beaucoup les ")
+        let tornadoAttachment = NSTextAttachment(image: tornado)
+        let tornadoAttributedString = NSAttributedString(attachment: tornadoAttachment)
+        attributedString.append(tornadoAttributedString)
+        label.attributedText = attributedString
     }
 
 
