fd601223a30253f9f974f123dbb2f7f0d5e993cf
Maxime Britto <maxime@purplegiraffe.fr>
2019-07-16 18:08:37 +0200

    142_Utiliser_depuis_le_Storyboard

---
+++ SFSymbols_demo/SFSymbols_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SFSymbols_demo
+//
+//  Created by Maxime Britto on 16/07/2019.
+//  Copyright © 2019 Maxime Britto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
