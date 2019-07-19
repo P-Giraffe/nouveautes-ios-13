5abe827888860274ca15c536f43dd49ed5419273
Maxime Britto <maxime@purplegiraffe.fr>
2019-07-19 17:53:00 +0200

    182_Utiliser_Xcode_avec_SwiftUI

---
+++ SwiftUI_demo/SwiftUI_demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI_demo
+//
+//  Created by Maxime Britto on 19/07/2019.
+//  Copyright © 2019 Maxime Britto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Purple Giraffe FTW!")
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
